cd166297e23e751d32c67cdeb8cbd3bb3523646c
JordanSh <yarden.shalom9@gmail.com>
2023-07-23 03:54:16 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { GbmSider } from './components/GbmSider';
+import { Routes, Route, Outlet, Link, BrowserRouter } from "react-router-dom";
+import { paths } from './constants/paths';
+import { BlogPage } from './pages/BlogPage';
 
-function App() {
+const { Header, Content, Footer, Sider } = Layout;
+
+const Page: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+      <Layout style={{ minHeight: '100vh' }}>
+        <GbmSider />
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
 
-export default App;
+
+            <Routes>
+              <Route path="/" >
+                <Route path={paths.home} element={<div>home</div>} />
+                <Route path={paths.blog} element={<BlogPage />} />
+                <Route path={paths.contact} element={<div>contact</div>} />
+              </Route>
+            </Routes>
+
+          </Content>
+          <Footer style={{}}>My Blog ©2023 Created by -EnterName-</Footer>
+        </Layout>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default Page;
